795212ef6446c977ed95bf8bb28b371fb238b481
Castro1105 <142284903+Castro1105@users.noreply.github.com>
2023-11-28 23:57:57 -0600

    Add files via upload

---
+++ responsive-review-site/java.js
@@ -0,0 +1,13 @@
+function change01() {
+    document.getElementById("large").src= "images/one.jpeg";
+}
+function change02() {
+    document.getElementById("large") .src= "images/two.jpeg";
+}
+function change03(){
+    document.getElementById("large") .src= "images/three.jpeg";
+
+}
+function change04() {
+    document.getElementById("large") .src= "images/four.JPG";
+}
